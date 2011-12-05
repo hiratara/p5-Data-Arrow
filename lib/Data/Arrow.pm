545d1954777fe42512c3e0527bb945bc52f72234
Masahiro Honma <hiratara@cpan.org>
2011-12-05 19:05:54 +0900

    initial commit

---
+++ lib/Data/Arrow.pm
@@ -0,0 +1,32 @@
+package Data::Arrow;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Data::Arrow -
+
+=head1 SYNOPSIS
+
+  use Data::Arrow;
+
+=head1 DESCRIPTION
+
+Data::Arrow is
+
+=head1 AUTHOR
+
+hiratara E<lt>hiratara {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
